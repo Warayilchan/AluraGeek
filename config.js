3db7c7063ec22a4388600c7341ac0723eeb487e1
Larissa S. Castro <larisousacastro@gmail.com>
2025-01-11 13:30:36 -0300

    Atualização do código para deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let API_BASE_URL = "http://localhost:3000"; 
 
-if (window.location.hostname === 'alura-geek-9km3odcr1-larissa-s-castros-projects.vercel.app') {
-    API_BASE_URL = "https://alura-geek-9km3odcr1-larissa-s-castros-projects.vercel.app";
+if (window.location.hostname === 'alura-geek-9jd1bboe0-larissa-s-castros-projects.vercel.app') {
+    API_BASE_URL = "https://alura-geek-9jd1bboe0-larissa-s-castros-projects.vercel.app/";
 }
 
 export { API_BASE_URL };
