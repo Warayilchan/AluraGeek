f40f49f24e6da8f9ef1421011d8bea8a2c83abe1
Larissa S. Castro <larisousacastro@gmail.com>
2025-01-11 14:34:23 -0300

    Uso de ngrok para deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let API_BASE_URL = "http://localhost:3000"; 
 
 if (window.location.hostname === 'alura-geek-9jd1bboe0-larissa-s-castros-projects.vercel.app') {
-    API_BASE_URL = "https://alura-geek-9jd1bboe0-larissa-s-castros-projects.vercel.app/";
+    API_BASE_URL = "https://e028-2804-d55-8faa-1400-21f2-be3c-37eb-eed.ngrok-free.app";
 }
 
 export { API_BASE_URL };
