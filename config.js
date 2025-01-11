238dbde97406e349ed5350c70e08a37fc095f297
Larissa S. Castro <larisousacastro@gmail.com>
2025-01-11 14:58:34 -0300

    Atualização do link

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let API_BASE_URL = "http://localhost:3000"; 
 
-if (window.location.hostname === 'alura-geek-9jd1bboe0-larissa-s-castros-projects.vercel.app') {
-    API_BASE_URL = "https://alura-geek-9jd1bboe0-larissa-s-castros-projects.vercel.app/";
+if (window.location.hostname === 'alura-geek-2djl8fi2e-larissa-s-castros-projects.vercel.app') {
+    API_BASE_URL = "https://alura-geek-2djl8fi2e-larissa-s-castros-projects.vercel.app/";
 }
 
 export { API_BASE_URL };
