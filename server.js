67be9652b359d560fc1b24fdd19e6be92dc8991b
Larissa S. Castro <larisousacastro@gmail.com>
2025-01-11 13:55:02 -0300

    Atualização para resolução de problemas no deploy do Vercel

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+    static: './public',
+    noCors: false,
+});
 
-server.use(middlewares);
-
+// Permitir acesso CORS
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Private-Network', 'true');
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
+server.use(middlewares);
 server.use(router);
 
 server.listen(3000, () => {
-    console.log('JSON Server is running on http://localhost:3000');
+    console.log('JSON Server is running');
 });
+
