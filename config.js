73ab901c655da548b873d9d14b6c07dac53ccbc8
Larissa S. Castro <larisousacastro@gmail.com>
2025-01-11 13:06:10 -0300

    Atualização do link de deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let API_BASE_URL = "http://localhost:3000"; 
 
 if (process.env.NODE_ENV === 'production') {
-    API_BASE_URL = "https://alura-geek-9km3odcr1-larissa-s-castros-projects.vercel.app/";
+    API_BASE_URL = "https://alura-geek-sigma-nine.vercel.app/";
 }
 
 export { API_BASE_URL };
