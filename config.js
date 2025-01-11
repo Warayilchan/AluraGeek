d53328d5416b5c44a9f7f2c5d37f54d6f16ad101
Larissa S. Castro <larisousacastro@gmail.com>
2025-01-11 15:24:27 -0300

    Atualizando para novo deploy no Netlify

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let API_BASE_URL = "http://localhost:3000"; 
 
-if (window.location.hostname === 'alura-geek-2djl8fi2e-larissa-s-castros-projects.vercel.app') {
-    API_BASE_URL = "https://alura-geek-2djl8fi2e-larissa-s-castros-projects.vercel.app/";
+if (window.location.hostname === 'app.netlify.com/sites/transcendent-sunflower-39d62d/deploys/6782b68f0979b5ed25c66024') {
+    API_BASE_URL = "https://app.netlify.com/sites/transcendent-sunflower-39d62d/deploys/6782b68f0979b5ed25c66024";
 }
 
 export { API_BASE_URL };
